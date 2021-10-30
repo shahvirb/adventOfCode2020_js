cc83166ab08d2abc30719aecea24d8e46dbb72cf
shahvirb <shahvirb@gmail.com>
2021-10-30 13:58:15 -0500

    completed day 2 part 2

--- day2.js
+++ day2.js
@@ -5,22 +5,29 @@ class PasswordLine {
     const minMax = elements[0].split('-').map(s => parseInt(s))
 
     this.policyChar = elements[1][0]
-    this.policyCharMin = minMax[0]
-    this.policyCharMax = minMax[1]
+    this.policyCharLo = minMax[0]
+    this.policyCharHi = minMax[1]
     this.password = elements[2]
   }
 
-  check () {
+  checkOldJob () {
     const count = [...this.password].filter(x => x === this.policyChar).length
-    return (count >= this.policyCharMin && count <= this.policyCharMax)
+    return (count >= this.policyCharLo && count <= this.policyCharHi)
+  }
+
+  checkTobogganCorporate () {
+    return (this.password[this.policyCharLo - 1] === this.policyChar) !== (this.password[this.policyCharHi - 1] === this.policyChar)
   }
 }
 
 const readfile = require('./readfile')
 const lines = readfile.readLines('day2.txt', line => new PasswordLine(line))
 
-let count = 0
+let c1 = 0
+let c2 = 0
 for (const line of lines) {
-  if (line.check()) count++
+  if (line.checkOldJob()) c1++
+  if (line.checkTobogganCorporate()) c2++
 }
-console.log(count)
+console.log(`Part 1 answer: ${c1}`)
+console.log(`Part 2 answer: ${c2}`)

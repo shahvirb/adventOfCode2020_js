10f0e6d271eebce21df2d790bb441e52ddeb8f7b
shahvirb <shahvirb@gmail.com>
2021-10-30 12:45:38 -0500

    run eslint with github standard style

--- day1.js
+++ day1.js
@@ -1,14 +1,14 @@
-const readfile = require('./readfile');
-numbers = readfile.readLines('day1.txt', str => parseInt(str));
+const readfile = require('./readfile')
+const numbers = readfile.readLines('day1.txt', str => parseInt(str))
 
-function findSum(numbers, sum) {
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < numbers.length; j++) {
-            if ((i != j) && (numbers[i] + numbers[j] == sum)) {
-                return numbers[i] * numbers[j];
-            }
-        }
+function findSum (numbers, sum) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if ((i !== j) && (numbers[i] + numbers[j] === sum)) {
+        return numbers[i] * numbers[j]
+      }
     }
+  }
 }
 
-console.log(findSum(numbers, 2020));
+console.log(findSum(numbers, 2020))
--- readfile.js
+++ readfile.js
@@ -1,20 +1,20 @@
-function readFile(filepath) {
-    const fs = require('fs');
-    try {
-        const data = fs.readFileSync(filepath, 'utf8');
-        return data;
-    } catch (err) {
-        console.error(err);
-    }
+function readFile (filepath) {
+  const fs = require('fs')
+  try {
+    const data = fs.readFileSync(filepath, 'utf8')
+    return data
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-function readLines(filepath, perLineFunc = null) {
-    let lines = readFile(filepath).split("\n");
-    if (perLineFunc) lines = lines.map(perLineFunc);
-    return lines;
+function readLines (filepath, perLineFunc = null) {
+  let lines = readFile(filepath).split('\n')
+  if (perLineFunc) lines = lines.map(perLineFunc)
+  return lines
 }
 
 module.exports = {
-    readFile: readFile,
-    readLines: readLines
+  readFile: readFile,
+  readLines: readLines
 }
